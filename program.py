dcbb0e237052c27ee8d3c61fc7461788e30da3cf
zach0987654321 <tracywolf05@outlook.com>
2023-11-02 14:23:20 -0500

    k

--- program.py
+++ program.py
@@ -1,19 +1,22 @@
-print('N')
-for num in range(100):
-    print('I')
-    
-print('N')
-print('T')
-print('E')
-print('N')
-print('D')
+def program():
+    import time
 
-for num in range(15):
-    print('O')
+    print('N')
+    for num in range(300):
+        print('I')
     
-time.sleep(0.8)
-print('W')
-print('A')
-print('H')
-for num in range(110):
-    print('0')
+    print('N')
+    print('T')
+    print('E')
+    print('N')
+    print('D')
+
+    for num in range(15):
+        print('O')
+    
+    time.sleep(0.8)
+    print('W')
+    print('A')
+    print('H')
+    for num in range(350):
+        print('O')
