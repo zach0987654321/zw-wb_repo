a0b9f191036fc1496d96155d0ce380ec3b235164
Blastzard <142840435+Blastzard@users.noreply.github.com>
2023-11-03 14:41:38 -0500

    Update program.py

--- program.py
+++ program.py
@@ -1,17 +1,36 @@
 def main():
     RANGEMAX=1000
     RANGEMIN=1
-    print(player1, player2)
     choice=1
-    while choice!=3:
+    while choice!=1:
         choice=menu()
         if choice==2:
-            RANGEMAX=ranged_randomint()
-            RANGEMIN=ranged_randomint()
+            ranged=choose_range()
+            RANGEMAX=ranged[1]
+            RANGEMIN=ranged[0]
         elif choice==1:
             player=player_name()
             player1=player[0]
             player2=player[1]
         elif choice==3:
             exit
+        else:
+            print("YOU DID NOT TYPE A VALID NUMBER")
+            main()
+    number=ranged_random_int()
+    guess_game(number,player1, player2)
+def guess_game(number,player1,player2):
+    roundnumber=1
+    while answer1!=number, or answer2!=number:
+        print("Round", roundnumber)
+        answer1=input(float("What is the random number", player1,"?"))
+        answer1=round(answer1)
+        answer2=input(float("What is the random number", player2,"?"))
+        answer1=round(answer1)
+        roundnumber+=1
+    if answer1==number:
+        print("YOU WIN", player1)
+    elif answer2==number:
+        print("YOU WIN", player2)
+
             
